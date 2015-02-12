3ae631680f924fbd99d367fd2967b11081ab80c8
AthosXtreme <AthosXtreme@gmail.com>
2015-02-12 16:33:19 -0300

    DateTime2 convention
    Convention added

---
+++ Solution/Brainary.Commons.Data.Ef/Conventions/DateTime2Convention.cs
@@ -0,0 +1,13 @@
+﻿namespace Brainary.Commons.Data.Conventions
+{
+    using System;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+
+    public class DateTime2Convention : Convention
+    {
+        public DateTime2Convention()
+        {
+            Properties<DateTime>().Configure(c => c.HasColumnType("datetime2"));
+        }
+    }
+}
