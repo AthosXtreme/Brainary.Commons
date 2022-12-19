e05993f6fc01e613f30b689144c036092ff0f16a
AthosXtreme <AthosXtreme@gmail.com>
2022-12-19 02:47:21 -0300

    Add bool.ToString extension overload

--- Solution/Brainary.Commons/Extensions/Misc.cs
+++ Solution/Brainary.Commons/Extensions/Misc.cs
@@ -1,4 +1,3 @@
-
 namespace Brainary.Commons.Extensions
 {
     public static partial class Extensions
@@ -12,6 +11,33 @@ namespace Brainary.Commons.Extensions
                 throw new ArgumentNullException(nameof(items));
 
             return items.Contains(value);
+        }
+
+        /// <summary>
+        /// Returns custom True/False/Null strings
+        /// </summary>
+        /// <param name="value">Bool value</param>
+        /// <param name="trueString">True string</param>
+        /// <param name="falseString">False string</param>
+        /// <param name="nullString">Null string</param>
+        /// <returns>Deined custom string for each case</returns>
+        public static string ToString(this bool? value, string trueString, string falseString, string nullString = "Undefined")
+        {
+            return value == null ? nullString : value.Value ? trueString : falseString;
+        }
+
+        /// <summary>
+        /// Returns custom True/False strings
+        /// </summary>
+        /// <param name="value">Bool value</param>
+        /// <param name="trueString">True string</param>
+        /// <param name="falseString">False string</param>
+        /// <returns>Deined custom string for each case</returns>
+        public static string ToString(this bool value, string trueString, string falseString)
+        {
+#pragma warning disable CS8625
+            return ToString(value, trueString, falseString, null);
+#pragma warning restore CS8625
         }
     }
 }
