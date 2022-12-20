2ce6109dbe29d0dd58c284e82e2d4d67a8285e1f
AthosXtreme <AthosXtreme@gmail.com>
2022-12-20 20:07:23 -0300

    Add Misc helper class

---
+++ Solution/Brainary.Commons/Util/Misc.cs
@@ -0,0 +1,18 @@
+﻿namespace Brainary.Commons.Util
+{
+    public static class Misc
+    {
+        /// <summary>
+        /// Generates random filename (without extension)
+        /// </summary>
+        /// <param name="pre">Optional prefix</param>
+        /// <returns>FileName string</returns>
+        public static string CreateFileName(string? pre = null)
+        {
+            pre = !string.IsNullOrWhiteSpace(pre) ? $"{pre}_" : string.Empty;
+            var hash = Math.Abs(Guid.NewGuid().GetHashCode());
+            var ends = Path.GetRandomFileName().Split('.');
+            return $"{pre}{ends[0]}_{hash}_{ends[1]}";
+        }
+    }
+}
