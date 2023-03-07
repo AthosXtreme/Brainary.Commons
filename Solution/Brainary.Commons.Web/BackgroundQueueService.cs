acdf5a32e7862a066f4af577996ece8a1b0863c1
AthosXtreme <AthosXtreme@gmail.com>
2023-03-06 21:18:14 -0300

    Add background queue service and worker

---
+++ Solution/Brainary.Commons.Web/BackgroundQueueService.cs
@@ -0,0 +1,38 @@
+﻿using Brainary.Commons.Util;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Brainary.Commons.Web
+{
+    public class BackgroundQueueService : BackgroundService
+    {
+        private readonly BackgroundQueueWorker queue;
+        private readonly ILogger<BackgroundQueueService> logger;
+
+        public BackgroundQueueService(BackgroundQueueWorker queue, ILogger<BackgroundQueueService> logger)
+        {
+            this.queue = queue;
+            this.logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var workItem = await queue.Dequeue(stoppingToken);
+
+                if (workItem != null)
+                {
+                    try
+                    {
+                        await workItem(stoppingToken);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "An exception occurred in a background queue task.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Solution/Brainary.Commons/Util/BackgroundQueueWorker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+
+namespace Brainary.Commons.Util
+{
+    public class BackgroundQueueWorker
+    {
+        private readonly ConcurrentQueue<Func<CancellationToken, Task>> workItems = new();
+        private readonly SemaphoreSlim signal = new(0);
+
+        public async Task<Func<CancellationToken, Task>?> Dequeue(CancellationToken cancellationToken)
+        {
+            await signal.WaitAsync(cancellationToken);
+            workItems.TryDequeue(out var workItem);
+
+            return workItem;
+        }
+
+        public void Enqueue(Func<CancellationToken, Task> workItem)
+        {
+            if (workItem == null)
+                throw new ArgumentNullException(nameof(workItem));
+
+            workItems.Enqueue(workItem);
+            signal.Release();
+        }
+    }
+}
