5c4eb5b0a63d16c8005e15895f8f6555966f1f41
AthosXtreme <AthosXtreme@gmail.com>
2023-03-19 22:55:23 -0300

    Add RolesAttribute

---
+++ Solution/Brainary.Commons.Web/RolesAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Brainary.Commons.Web
+{
+    /// <summary>
+    /// <see cref="AuthorizeAttribute"/> implementation for roles by default.
+    /// </summary>
+    public class RolesAttribute : AuthorizeAttribute
+    {
+        public RolesAttribute() { }
+
+        public RolesAttribute(params string[] roles)
+        {
+            Roles = string.Join(",", roles);
+        }
+    }
+}
